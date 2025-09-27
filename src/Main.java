0eb6ca218c6b625619fee7ede6ced1bd3a0a5966
Maxim Sergeevich <flik883@gmail.com>
2025-09-27 20:16:04 +0700

    feat: implemented the ability to obtain even and odd numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        OddEvenSeparator separator = new OddEvenSeparator();
+        separator.addNumber(9147);
+        separator.addNumber(234);
+        separator.addNumber(2128194124);
+        separator.addNumber(123);
+
+        separator.even();
+        separator.odd();
     }
 }
