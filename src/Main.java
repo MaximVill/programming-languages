064b6a222711e35038b3264cf72ad0d93f1ad2b1
Maxim Sergeevich <flik883@gmail.com>
2025-09-27 19:39:44 +0700

    feat: implemented comparison

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Balance balance1 = new Balance();
+        balance1.addRight(3);
+        balance1.addLeft(2);
+        balance1.result();
+
+        Balance balance2 = new Balance();
+        balance2.addRight(2);
+        balance2.addLeft(3);
+        balance2.result();
+
+        Balance balance3 = new Balance();
+        balance3.addRight(3);
+        balance3.addLeft(3);
+        balance3.result();
     }
 }
