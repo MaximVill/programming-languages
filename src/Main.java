99522e9fd58f7b9dd0dde5d769f72dba57baa9f7
Maxim Sergeevich <flik883@gmail.com>
2025-09-27 20:02:54 +0700

    feat: sound playback has been implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Bell bell = new Bell();
+        bell.sound();
+        bell.sound();
     }
 }
