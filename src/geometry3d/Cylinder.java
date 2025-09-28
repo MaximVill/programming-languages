5c0dc945089b962afa38f6a19e1150a4cb0b348f
Maxim Sergeevich <flik883@gmail.com>
2025-09-28 15:57:32 +0700

    feat: implemented the geometry3d package with the Cylinder class

--- src/geometry3d/Cylinder.java
+++ src/geometry3d/Cylinder.java
@@ -1,4 +1,28 @@
 package geometry3d;
+import geometry2d.Figure;
+import exceptions.InvalidDimensionException;
+import exceptions.NullFigureException;
 
 public class Cylinder {
+    Figure base; // Основание
+    double height;
+
+    public Cylinder(Figure base, double height) {
+        if (base == null) {
+            throw new NullFigureException("Base figure cannot be null");
+        }
+        if (height <= 0) {
+            throw new InvalidDimensionException("Height must be positive: " + height);
+        }
+        this.base = base;
+        this.height = height;
+    }
+
+    public double volume() {
+        return base.area() * height;
+    }
+
+    public String toString() {
+        return String.format("Cylinder base, height, volume = ", base, height, volume());
+    }
 }
