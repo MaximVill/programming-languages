978b97cd9f10bedabba2190f512bb052666e85c7
Maxim Sergeevich <flik883@gmail.com>
2025-09-28 15:11:25 +0700

    feat: implemented the Table class output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Table table = new Table(3, 3);
+
+        table.set_value(0, 0, 1);
+        table.set_value(1, 1, 2);
+        table.set_value(2, 2, 8);
+
+        System.out.println(table.toString());
+        System.out.println("");
+
+        System.out.println("Average: " + table.average()); // 1.(3)
+        System.out.println("Rows: " + table.rows());      // 3
+        System.out.println("Cols: " + table.cols());      // 3
     }
 }
