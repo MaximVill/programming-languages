1a1728cada5914ea621c1d5de8a7651bcd1764be
Maxim Sergeevich <flik883@gmail.com>
2025-09-28 16:17:55 +0700

    feat: implemented the output of all figures with the given parameters

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import geometry2d.*;
+import geometry3d.Cylinder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Circle circle = new Circle(3.0);
+        Rectangle rectangle = new Rectangle(2.5, 5.9);
+        Cylinder cylinder1 = new Cylinder(circle, 4.2);
+        Cylinder cylinder2 = new Cylinder(rectangle, 5.2);
+
+        System.out.println(circle);
+        System.out.println(rectangle);
+        System.out.println(cylinder1);
+        System.out.println(cylinder2);
     }
 }
