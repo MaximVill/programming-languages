c49ecfb92cdec08641bc75480ecfe682f3bd3c4c
Maxim Sergeevich <flik883@gmail.com>
2025-10-01 21:30:13 +0700

    feat: performed a cyclic shift by 1 element

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             massive[i] = (int)(Math.random() * 100); // заполняем массив числами от 0 до 100
-            System.out.println(massive[i]);
+            // System.out.println(massive[i]);
         }
 
         // создаю список на основе массива
@@ -20,18 +20,22 @@ public class Main {
         for (int value : massive) {
             List.add(value);
         }
-        // System.out.println(List);
+        System.out.println(List);
 
         // Сортирую список по возрастанию
-        Collections.sort(List);
+        // Collections.sort(List);
         // System.out.println(List);
 
         // Сортирую список в обратном порядке
-        Collections.sort(List, Collections.reverseOrder());
+        // Collections.sort(List, Collections.reverseOrder());
         // System.out.println(List);
 
         // Перемешиваю список
-        Collections.shuffle(List);
+        // Collections.shuffle(List);
+        // System.out.println(List);
+
+        // Технический сдвиг на 1
+        Collections.rotate(List, 1);
         System.out.println(List);
     }
 }
