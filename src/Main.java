8aa771229a4a41e6fc38f36189ca22c7a0c294cd
Maxim Sergeevich <flik883@gmail.com>
2025-10-04 20:57:45 +0700

    feat: implemented the output in direct and reverse order

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+// Класс PrimesGenerator генерирует N простых чисел по одному
 class PrimeGenerator implements Iterable<Integer> {
-    private final int N;
+    private final int N; // Сколько простых чисел нужно
+
+    // запоминает, сколько простых чисел нужно.
     public PrimeGenerator(int N) {
         this.N = N;
     }
@@ -11,9 +14,10 @@ class PrimeGenerator implements Iterable<Integer> {
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
-            private int count = 0;
-            private int current = 2;
+            private int count = 0; // Сколько простых чисел уже есть
+            private int current = 2; // Это первое простое число, т.к 1 не может им быть
 
+            // Движемся дальше, если количество простых чисел не превышает заданного количества
             @Override
             public boolean hasNext() {
                 return count < N;
@@ -21,27 +25,31 @@ class PrimeGenerator implements Iterable<Integer> {
 
             @Override
             public Integer next() {
-                if (!hasNext()) {
+                if (!hasNext()) { // Проверка на исключение
                     throw new NoSuchElementException();
                 }
+
+                // пока число не окажется простым двигаемся дальше
                 while (!isPrime(current)){
                     current++;
                 }
-                int prime = current;
-                current++;
-                count++;
+
+                int prime = current; // запоминаем простое число
+                current++; // новое крайнее простое число
+                count++; // увеличиваем счетчик найденных
                 return prime;
 
             }
 
+            // проверка на просто число
             private boolean isPrime(int x) {
-                if (x < 2) return false;
+                if (x < 2) return false; // 0 и 1 не простые числа
                 for (int i = 2; i < x; i++) {
                     if (x % i == 0) {
-                        return false;
+                        return false; // если у числа есть делитель, значит оно составное
                     }
                 }
-                return true;
+                return true; // если число обошло проверку, то оно простое
             }
         };
     }
@@ -52,5 +60,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Сколько простых чисел вывести?\n>> ");
         int N = scanner.nextInt();
+
+        PrimeGenerator generator = new PrimeGenerator(N);
+        int[] primes = new int[N];
+        int index = 0;
+        for (int prime : generator) {
+            primes[index] = prime;
+            index++;
+        }
+
+        System.out.println("Прямой порядок: ");
+        for (int num : primes) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Обратный порядок: ");
+        for (int i = primes.length - 1; i >= 0; i--) {
+            System.out.print(primes[i] + " ");
+        }
     }
 }
