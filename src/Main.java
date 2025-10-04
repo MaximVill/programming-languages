b895727b6fc0672cdb8660717c0c845ad4cb995f
Maxim Sergeevich <flik883@gmail.com>
2025-10-04 21:15:38 +0700

    feat: developed the Human class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+class Human {
+    String firstName;
+    String lastName;
+    String age;
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
