b79acc8a0f6f17d78f760b2aa48a6ac4663537bf
Maxim Sergeevich <flik883@gmail.com>
2025-10-11 13:06:31 +0700

    feat: sorted and displayed a list of all books by year of publication.

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.io.FileReader;
 import java.lang.reflect.Type;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,6 +48,16 @@ public class Main {
         for (int i = 0; i < uniqueBooks.size(); i++){
             Book b = uniqueBooks.get(i);
             System.out.println((i + 1) + ") " + b.getName() + " — " + b.getAuthor()); // 20 уникальных книг в избранном
+        }
+
+        // №3. Отсортировать и вывести список всех книг по году издания.
+        System.out.println("\nЗАДАНИЕ 3: ОТСОРТИРОВАННЫЙ СПИСОК ВСЕХ КНИГ ПО ГОДУ ИЗАДАНИЯ");
+        List<Book> allBooksSorted = visitors.stream().flatMap(v -> v.getFavoriteBooks().stream())
+                .sorted(Comparator.comparingInt(Book::getPublishingYear))
+                .collect(Collectors.toList());
+        for (int i = 0; i < allBooksSorted.size(); i++){
+            Book b = allBooksSorted.get(i);
+            System.out.println((i + 1) + ") " + b.getPublishingYear() + " — " + b.getName()); // 20 уникальных книг в избранном
         }
     }
 }
