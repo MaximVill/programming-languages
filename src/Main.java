b969b9939f8bfda90027cebcb4b59b54434fe08d
Maxim Sergeevich <flik883@gmail.com>
2025-10-11 12:20:11 +0700

    feat: output variables for parsing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import lombok.Data;
+import java.util.List;
+
+// Класс книги
+@Data
+class Book {
+    private String name;
+    private String author;
+    private int publishingYear;
+    private String isbn;
+    private String publisher;
+}
+
+// Класс посетителя
+@Data
+class Visitor {
+    private String name;
+    private String surname;
+    private String phone;
+    private boolean subscribed;
+    private List<Book> favoriteBooks;
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
