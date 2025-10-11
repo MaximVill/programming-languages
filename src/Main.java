51774927895aa5fc68a65e52dd2a6c9d7e1f5663
Maxim Sergeevich <flik883@gmail.com>
2025-10-11 12:39:03 +0700

    feat: displayed a list of visitors and their number.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import lombok.Data;
+
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.List;
 
 // Класс книги
@@ -22,6 +27,16 @@ class Visitor {
 }
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<Visitor>>(){}.getType();
+        List<Visitor> visitors = gson.fromJson(new FileReader("books.json"), listType);
+
+        // №1. Вывести список посетителей и их количество.
+        System.out.println("ЗАДАНИЕ 1: СПИСОК ПОСЕТИТЕЛЕЙ И ИХ КОЛИЧЕСТВО");
+        for (int i = 0; i < visitors.size(); i++) {
+            Visitor v = visitors.get(i);
+            System.out.println((i + 1) + ") " + v.getName() + " " + v.getSurname());
+        }
     }
 }
