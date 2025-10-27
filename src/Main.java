90d01c2a79ed4ae809319ff24ff34decaa04f0dd
Maxim Sergeevich <flik883@gmail.com>
2025-10-27 23:11:34 +0700

    feat: implement FileSystemManager

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=".repeat(50));
-        System.out.println("ЛАБОРАТОРНАЯ РАБОТА №6");
-        System.out.println("Рефлексия, аннотации и работа с файловой системой");
-        System.out.println("=".repeat(50));
-
-        // === ЗАДАНИЕ 1 ===
+         // №1. Рефлексия и аннотации
         System.out.println("\n" + "=".repeat(50));
         System.out.println("ЗАДАНИЕ 1: Рефлексия и аннотации");
         System.out.println("=".repeat(50));
@@ -17,6 +12,16 @@ public class Main {
             Invoker.main(new String[]{});
         } catch (Exception e) {
             System.err.println("Ошибка в задании 1: " + e.getMessage());
+        }
+
+        // №2. Работа с файловой системой
+        System.out.println("\n" + "=".repeat(50));
+        System.out.println("ЗАДАНИЕ 2: Работа с файловой системой");
+        System.out.println("=".repeat(50));
+        try {
+            FileSystemManager.main(new String[]{});
+        } catch (Exception e) {
+            System.err.println("Удалите папку Vill в корне проекта");
         }
 
         scanner.close();
